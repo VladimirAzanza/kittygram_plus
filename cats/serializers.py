6180903df0d697bd65a630536fa3c45891c40134
Vladimir Azanza <vladimirazanza@gmail.com>
2024-07-25 11:35:00 +0300

    Add new field 'age' to cat serializer using SerializerMethodField

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from rest_framework import serializers
 
 from .models import Achievement, AchievementCat, Cat, Owner
@@ -7,7 +9,10 @@ class AchievementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = (
+            'id',
+            'name'
+        )
 
 
 class CatSerializer(serializers.ModelSerializer):
@@ -20,10 +25,19 @@ class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(
         many=True, required=False,
     )
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = (
+            'id',
+            'name',
+            'color',
+            'birth_year',
+            'owner',
+            'achievements',
+            'age'
+        )
 
     def create(self, validated_data):
         if 'achievements' not in self.initial_data:
@@ -41,10 +55,17 @@ class CatSerializer(serializers.ModelSerializer):
                 achievement=current_achievement, cat=cat)
         return cat
 
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
 
 class OwnerSerializer(serializers.ModelSerializer):
     cats = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Owner
-        fields = ('first_name', 'last_name', 'cats')
+        fields = (
+            'first_name',
+            'last_name',
+            'cats'
+        )
